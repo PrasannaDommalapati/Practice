be82d8a4e364012710d12f8735ec6d23ffdc6ba0
unknown <in02107@HW1611.cigniti.local>
2016-03-09 16:55:12 -0600

    Made changes to the ExcelToXml.java and ExcelUtility.java files - Added SOPL statements

--- src/main/java/com/automation/utilities/ExcelToXml.java
+++ src/main/java/com/automation/utilities/ExcelToXml.java
@@ -5,6 +5,7 @@ public class ExcelToXml {
 	public static void main(String[] args) throws Exception
 	{
 		try{
+		System.out.println("In the ExceltoXML class");
 		ExcelUtility.createXml("Browsers");
 		
 			
