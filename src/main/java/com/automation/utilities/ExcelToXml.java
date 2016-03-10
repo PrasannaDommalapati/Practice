73cc1774c196b5eb67a554155b4eff64667d5784
unknown <in02107@HW1611.cigniti.local>
2016-03-10 10:17:37 -0600

    Everything good....Please ignore all these commits

--- src/main/java/com/automation/utilities/ExcelToXml.java
+++ src/main/java/com/automation/utilities/ExcelToXml.java
@@ -4,9 +4,9 @@ public class ExcelToXml {
 
 	public static void main(String[] args) throws Exception {
 		try {
-			System.out.println("***************Successfully entered the create xml method*********");
+			System.out.println("***************Successfully entered the create xml  - Need to remove these statements*********");
 			ExcelUtility.createXml("Browsers");
-			System.out.println("***************Successfull completed the create xml method*********");
+			System.out.println("***************Successfull completed the create xml method   - Need to remove these statements*********");
 
 		} catch (Exception ex) {
 			ex.printStackTrace();
