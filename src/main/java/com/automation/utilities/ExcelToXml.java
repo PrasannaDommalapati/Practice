4bdc63e44ec4d04865d7d1c94feaf416a0c748cf
unknown <in02107@HW1611.cigniti.local>
2016-03-10 09:37:29 -0600

    Updated the ExcelToXml.java file to test that the changes are indeed pulled

--- src/main/java/com/automation/utilities/ExcelToXml.java
+++ src/main/java/com/automation/utilities/ExcelToXml.java
@@ -5,6 +5,7 @@ public class ExcelToXml {
 	public static void main(String[] args) throws Exception
 	{
 		try{
+			System.out.println("In the ExcelToXml class of the project");
 		ExcelUtility.createXml("Browsers");
 		
 			
