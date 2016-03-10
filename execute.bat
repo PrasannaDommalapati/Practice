b468101287ab494ffc3e99343711b7fe0f601bf5
unknown <in02107@HW1611.cigniti.local>
2016-03-10 09:43:36 -0600

    Compiled yet again and testing whether the changes are pulled or not

--- execute.bat
+++ execute.bat
@@ -1,7 +1,7 @@
 echo "Mumms automation suite exceution"
 echo " ===creating dynamic xml from excel ==="
 
-cd D:\Automation\MummsFrameWork
+cd D:\GitHubServer
 
 java -cp .\\lib\\*;.\\target\\test-classes;.\\target\\classes; com.automation.utilities.ExcelToXml
 
--- src/main/java/com/automation/utilities/ExcelToXml.java
+++ src/main/java/com/automation/utilities/ExcelToXml.java
@@ -5,7 +5,7 @@ public class ExcelToXml {
 	public static void main(String[] args) throws Exception
 	{
 		try{
-			System.out.println("In the ExcelToXml class of the project");
+			System.out.println("****************In the ExcelToXml class of the project******************");
 		ExcelUtility.createXml("Browsers");
 		
 			
